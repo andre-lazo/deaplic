c09526c06cfc3333d9479d6bc6366ac4407ff9bf
andre-lazo <65989412+andre-lazo@users.noreply.github.com>
2020-11-14 22:35:16 -0500

    Update and rename resources/views/layouts/app.blade.php to recursos/puntos de vista/diseños/app.blade.php

--- recursos/puntos de vista/diseños/app.blade.php
+++ recursos/puntos de vista/diseños/app.blade.php
@@ -8,8 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>AdminLTE 3</title>
-
+<title> DEAPLIC </title>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
